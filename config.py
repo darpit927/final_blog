681a964706339c6303f58994807f62b7adda0cad
anandveeraswamy <anandveeraswamy@gmail.com>
2025-07-14 11:25:28 +0100

    update config to work with sqlite if no postgres found

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
 # When deploying, set in the environment to the PostgreSQL URL
-# SQLALCHEMY_DATABASE_URI = os.getenv('RENDER_DATABASE_URL', 'sqlite:///db.sqlite3')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+
